65e57fdd039d74e9dfe3f29233edd8808a92371d
kanadara13 <shin.jaehyeok@leisureq.co.kr>
2018-04-03 01:42:05 +0900

    [biz-ui] update table component

--- src/component/Table.js
+++ src/component/Table.js
@@ -1,48 +1,32 @@
 import React, { Component } from 'react';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 class Table extends Component {
     render() {
-        console.log(this.props.data)
-        let page = {}
         let header = this.props.header;
         let content = this.props.data.content
         return (
             <section className="content">
-                <div className="row">
-                    <div className="col-xs-12">
-                        <div className="box">
-                            <div className="box-header">
-                                <h3 className="box-title"></h3>
-                            </div>
-                            <div className="box-body">
-                                <table className="table table-bordered table-hover">
-                                    <thead>
-                                    <tr>
-                                        {header.map((each,index) => {
-                                            return <th key={index}>{each}</th>
-                                        })}
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    {content.map((each, index) => {
-                                        return <tr key={index}>
-                                            <td>{each.id}</td>
-                                            <td>{each.name}</td>
-                                            <td>{each.createdAtToString}</td>
-                                            <td>{each.updatedAtToString}</td>
-                                        </tr>
-                                    })}
-                                    </tbody>
-                                    <tfoot>
-                                    <tr>
-                                        {header.map((each,index) => {
-                                            return <th key={index}>{each}</th>
-                                        })}
-                                    </tr>
-                                    </tfoot>
-                                </table>
-                            </div>
+                <div className="col-xs-12">
+                    <div className="box">
+                        <div className="box-header">
+                            <h3 className="box-title">{this.props.title}</h3>
                         </div>
+                        <ReactTable
+                            data={content}
+                            columns={
+                                [
+                                   {
+                                    Header: "",
+                                    columns: header,
+                                }
+                            ]}
+                            defaultPageSize={10}
+                            filterable
+                            pages={this.props.data.totalPages}
+                            className="-striped -highlight"
+                        />
                     </div>
                 </div>
             </section>
--- src/material/Material.js
+++ src/material/Material.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import ContentHeader from '../component/ContentHeader';
 import Table from '../component/Table';
+import Alert from '../component/Alert';
+
+const headers = [
+    {
+        Header: "재료ID",
+        accessor: "id"
+    },
+    {
+        Header: "재료명",
+        id: "name",
+        accessor: d => d.name
+    },
+    {
+        Header: "등록일자",
+        accessor: "createdAtToString"
+    },
+    {
+        Header: "수정일자",
+        accessor: "updatedAtToString"
+    }
+]
+const title  = '재료 목록';
 
 class Material extends Component {
-
-    state  = {
-        header : ["재료ID", "재료명", "등록일자", "수정일자"]
-    };
+    state  = {};
 
     componentDidMount(){
         this.findMaterial();
@@ -14,28 +33,51 @@ class Material extends Component {
 
     findMaterial = async () => {
         const materials = await this.callAPi();
-        this.setState({
-             data : materials
-        })
+        if (materials === 'error') {
+            this.setState({
+                apiResult : false
+            })
+        }else{
+            this.setState({
+                 apiResult : true
+                ,data : materials.data
+            })
+        }
     }
 
 
     callAPi = async () =>{
-       return fetch("http://localhost:8080/v1/material")
-            .then(response => response.json())
-            .then(json => json.data)
-            .catch(err => console.log(err))
+            return fetch("http://localhost:8080/v1/material")
+                .then(response => response.json())
+                .then(json => json)
+                .catch(err => 'error');
     }
 
     renderTable = () => {
-        return <Table data={this.state.data} header={this.state.header}/>
+        return <Table data={this.state.data} header={headers} title={title}/>
+    }
+
+    renderAlert = (message) => {
+        return <Alert message={message}/>
+    }
+
+    renderButton = () => {
+        return <div className="box-footer">
+                <button onClick={this.handleClick} className="btn btn-info pull-right">등록</button>
+              </div>
+    }
+
+    handleClick = () => {
+        console.log('this is:', this);
     }
 
     render() {
         return (
             <div>
                 <ContentHeader category={"재료관리"} page={"재료목록"} description={"재료를 등록,수정, 삭제 관리합니다"} />
-                {this.state.data ? this.renderTable() : 'Loading'}
+                {this.state.apiResult === true  ? this.renderTable() : ''}
+                {this.state.apiResult === true  ? this.renderButton() : ''}
+                {this.state.apiResult === false ? this.renderAlert('서버와의 통신이 원할하지 않습니다') : ''}
             </div>
         );
     }
